666bdc6d39d125d07fc505f479465a04da34276b
soycoder <43911669+soycoder@users.noreply.github.com>
2021-04-24 17:29:59 +0700

    Create ToasterCard.js

---
+++ thaiegghead.com/src/components/ToasterCard.js
@@ -0,0 +1,19 @@
+import React, { useState} from "react";
+import { Button, Position, Toaster, Intent } from "@blueprintjs/core";
+
+const ToasterCard = () => {
+  const [toaster, setToaster] = useState([]);
+
+  function addToast() {
+    toaster.show({ message: "Toasted!", intent: Intent.DANGER });
+  }
+
+  return (
+    <div>
+      <Button onClick={addToast} text="Procure toast" />
+      <Toaster position={Position.TOP} ref={(ref) => setToaster(ref)} />
+    </div>
+  );
+};
+
+export default ToasterCard;
