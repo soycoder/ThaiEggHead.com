060ae569cb6a72765aed4db03c4602068b3dae72
ball5851 <69568067+ball5851@users.noreply.github.com>
2021-05-01 18:34:17 +0700

    + home layout

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -3,11 +3,43 @@ import bg from "./../assets/images/bg2.jpg";
 import bi from "./../assets/images/binary.jpg";
 import pic_cover from "./../assets/images/example-cover.jfif"
 import pic_profile from "./../assets/images/example-profile.jfif"
+import subj_1 from "./../assets/images/subject/1.jpg"
+import subj_2 from "./../assets/images/subject/2.jpg"
+import subj_3 from "./../assets/images/subject/3.png"
+import subj_4 from "./../assets/images/subject/4.jpg"
+import subj_5 from "./../assets/images/subject/5.png"
+import subj_6 from "./../assets/images/subject/6.jpg"
+import subj_7 from "./../assets/images/subject/7.png"
+import subj_8 from "./../assets/images/subject/8.jpg"
+import subj_9 from "./../assets/images/subject/9.png"
+import subj_10 from "./../assets/images/subject/10.png"
+import subj_11 from "./../assets/images/subject/11.png"
+import subj_12 from "./../assets/images/subject/12.jpg"
+import pro_1 from "./../assets/images/pro_1.jpg"
+import pro_2 from "./../assets/images/pro_2.jpg"
+import pro_3 from "./../assets/images/pro_3.jpg"
+import bina from "./../assets/images/binary.png"
 
 export default {
     logo,
     bg,
     bi,
     pic_cover,
-    pic_profile
+    pic_profile,
+    subj_1,
+    subj_2,
+    subj_3,
+    subj_4,
+    subj_5,
+    subj_6,
+    subj_7,
+    subj_8,
+    subj_9,
+    subj_10,
+    subj_11,
+    subj_12,
+    pro_1,
+    pro_2,
+    pro_3,
+    bina
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,105 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Row, Col, Card, Container, ListGroup, Form } from 'react-bootstrap';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
+import { images } from '../constants';
 
-const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-            <br/>
-            <li>
-                <NavLink to="/profile">Profile Page</NavLink>
-            </li>
-            <li>
-                <NavLink to="/CreateForum">Create Forum</NavLink>
-            </li>
-            <li>
-                <NavLink to="/subject">Subject Page</NavLink>
-            </li>
-        </div>
-    )
+function App() {
+  return (
+    <div>
+      <body style={{backgroundColor: "#F3F3F3"}}>
+      <br />
+      <br />
+      <Container>
+        <Row>
+          <Col>
+            <BrowserRouter>
+            <nav>
+              <ul>
+                <li><Link to='' ><img src={images.subj_1} height="23" width="23" className="app-cycle" /> Sciences</Link></li>
+                <li><Link to=''><img src={images.subj_2} height="23" width="23" className="app-cycle"/> Law</Link></li>
+                <li><Link to=''><img src={images.subj_3} height="23" width="23" className="app-cycle"/> Social Administration</Link></li>
+                <li><Link to=''><img src={images.subj_4} height="23" width="23" className="app-cycle"/> Political Science</Link></li>
+                <li><Link to=''><img src={images.subj_5} height="23" width="23" className="app-cycle"/> Engineering</Link></li>
+                <li><Link to=''><img src={images.subj_6} height="23" width="23" className="app-cycle"/> Communication Arts</Link></li>
+                <li><Link to=''><img src={images.subj_7} height="23" width="23" className="app-cycle"/> Social Work</Link></li>
+                <li><Link to=''><img src={images.subj_8} height="23" width="23" className="app-cycle"/> Education</Link></li>
+                <li><Link to=''><img src={images.subj_9} height="23" width="23" className="app-cycle"/> Commerce and <br/> Accountancy</Link></li>
+                <li><Link to=''><img src={images.subj_10} height="23" width="23" className="app-cycle"/> Fine and Applied Arts</Link></li>
+                <li><Link to=''><img src={images.subj_11} height="23" width="23" className="app-cycle"/> Arts</Link></li>
+                <li><Link to=''><img src={images.subj_12} height="23" width="23" className="app-cycle"/> Psychology</Link></li>
+              </ul>
+            </nav>
+            <Switch>
+
+            </Switch>
+            </BrowserRouter>
+          </Col>
+
+          <Col xs={7}>
+            <Card className="app-padding">
+              <Card.Subtitle className="mb-2 text-muted"><img src={images.pro_1} height="30" width="30" className="app-cycle"/> pop fever</Card.Subtitle>
+              <Form>
+                <Form.Group>
+                  <Form.Control placeholder= "คุณกำลังติดปัญหาอะไรรึเปล่า ? ถามมาสิ" />
+                </Form.Group>
+              </Form>
+            </Card>
+            <p/>
+            <Card className="app-padding">
+              <Card.Title><img src={images.pro_2} height="30" width="30" className="app-cycle"/> Compile error: Multiple definitions in int main</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">ฉันกำลังทำการบ้านและได้รับข้อผิดพลาดนี้ในเว็บไซต์ทดสอบของอาจารย์: /tmp/ccNzBwVD,o; ในฟัง์ชัน 'factorial (int)';myFunctions.cpp:(, text + 0x0); คำจำกัดความหลายคำ ...</Card.Subtitle>
+            </Card>
+            <p/>
+            <Card className="app-padding">
+              <Card.Title><img src={images.pro_3} height="30" width="30" className="app-cycle"/> หากคุณสร้างภาษาใหม่และต้องการเขียนคอมไพเลอร์ในภาษาเดียวกัน (เช่น C, Go และ Rust) คุณจะรวบรวมคอมไพเลอร์เป็นครั้งแรกโดยไม่มีคอมไพเลอร์ ได้อย่างไร?</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">ฉันกำลังทำการบ้านและได้รับข้อผิดพลาดนี้ในเว็บไซต์ทดสอบของอาจารย์: /tmp/ccNzBwVD,o; ในฟัง์ชัน 'factorial (int)';myFunctions.cpp:(, text + 0x0); คำจำกัดความหลายคำ ...</Card.Subtitle>
+              <img src={images.bina}/>
+            </Card>
+          </Col>
+
+          <Col>
+            <Card style={{ width: '13rem' }}>
+              <Card.Header>Custom Filter</Card.Header>
+              <Card.Body>
+                <Card.Link href="#">Create a custom filter</Card.Link>
+              </Card.Body>
+            </Card>
+            <br />
+            <Card style={{ width: '13rem' }}>
+              <Card.Header>
+                Watched Tags
+                <Card.Link href="#">Edit</Card.Link>
+              </Card.Header>
+              <Card.Body>
+                
+              </Card.Body>
+            </Card>
+            <br />
+            <Card style={{ width: '13rem' }}>
+              <Card.Header>Ignored Tags</Card.Header>
+              <Card.Body>
+                <Card.Link href="#">Add an ignored tag</Card.Link>
+              </Card.Body>
+            </Card>
+            <br />
+            <Card style={{ width: '13rem' }}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>Spaces to follow</ListGroup.Item>
+                <ListGroup.Item></ListGroup.Item>
+                <ListGroup.Item></ListGroup.Item>
+                <ListGroup.Item></ListGroup.Item>
+                <ListGroup.Item></ListGroup.Item>
+                <ListGroup.Item></ListGroup.Item>
+                <ListGroup.Item></ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      </body>
+    </div>
+  );
 }
 
-export default Home
-
-
+export default App;
