8d02964813430810262c2e16b3fb4b1e2acd86aa
soycoder <43911669+soycoder@users.noreply.github.com>
2021-05-01 19:06:02 +0700

    + server cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import path from "path";
+import cors from "cors";
 import mongooseDbConnect from "./config/database.js";
 
 import { default as user } from "./routes/UserRouter.js";
@@ -19,6 +20,7 @@ app.use("/public", express.static(path.join(process.cwd(), "public")));
 app.use(logger("short"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors())
 app.use(errorHandler);
 mongooseDbConnect();
 
