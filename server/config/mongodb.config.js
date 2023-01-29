081abdbb0f96ce8400e1beedab25169a1d6de5a7
apisith.won <om.apisith@gmail.com>
2023-01-29 11:04:08 +0700

    final update dependency

--- server/config/mongodb.config.js
+++ server/config/mongodb.config.js
@@ -1,4 +1,7 @@
-let dbURI = "mongodb://127.0.0.1:27017/thaiegghead";
+import dotenv  from "dotenv";
+dotenv.config()
+
+let dbURI = "mongodb://localhost:27017/thaiegghead";
 if (process.env.NODE_ENV === "production") {
   dbURI = process.env.MONGO_URI; // production DB server
 }
